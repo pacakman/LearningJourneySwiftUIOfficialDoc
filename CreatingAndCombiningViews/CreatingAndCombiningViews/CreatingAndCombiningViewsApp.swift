c865a93140da1d0e977656e7eb1a2cdd70306e14
Idris <idrisnovandi@klikdokter.com>
2024-05-13 22:08:24 +0700

    init project

---
+++ CreatingAndCombiningViews/CreatingAndCombiningViews/CreatingAndCombiningViewsApp.swift
@@ -0,0 +1,17 @@
+//
+//  CreatingAndCombiningViewsApp.swift
+//  CreatingAndCombiningViews
+//
+//  Created by Idris on 13/05/24.
+//
+
+import SwiftUI
+
+@main
+struct CreatingAndCombiningViewsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
